3c1868746e6979136445bd5838bb8f5dab317ef2
John Flockton <j.r.flockton@gmail.com>
2018-03-04 23:31:52 +0000

    replace webpack with rollup

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from "rollup-plugin-typescript2";
+import uglify from "rollup-plugin-uglify";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "lib/index.js",
+    format: "cjs",
+  },
+  context: "window",
+  plugins: [
+    typescript({
+      clean: true,
+      tsconfig: "tsconfig.prod.json",
+    }),
+    uglify(),
+  ],
+};
